326001b2f9b2713d70491bb4a547da59ef92f63f
duck <shangaoxiangnb@gmail.com>
2025-03-25 23:11:12 +0800

    添加标签下划线

--- config.def.h
+++ config.def.h
@@ -66,10 +66,13 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1; /* number of clients in master area */
-static const int resizehints =
-	1; /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen =
-	1; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
