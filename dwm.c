695bb6df5a71602beb321cbfe32929024bb40c6c
duck <shangaoxiangnb@gmail.com>
2025-05-05 16:43:25 +0800

    使用状态烂空隙布丁

--- dwm.c
+++ dwm.c
@@ -2172,8 +2172,8 @@ void setup(void)
 	drw = drw_create(dpy, screen, root, sw, sh);
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
-	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	lrpad = drw->fonts->h + horizpadbar;
+	bh = drw->fonts->h + vertpadbar + 2;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
